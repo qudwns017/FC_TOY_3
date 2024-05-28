74c84b0219260b9b6bdba665a61cd2b6ed2d07d9
junhankoo <junhankoo.kr@gmail.com>
2024-05-28 18:26:14 +0900

    Feat : 숙박 일부 수정

--- src/main/java/org/group6/travel/domain/accommodation/model/entity/AccommodationEntity.java
+++ src/main/java/org/group6/travel/domain/accommodation/model/entity/AccommodationEntity.java
@@ -2,6 +2,7 @@ package org.group6.travel.domain.accommodation.model.entity;
 
 import com.fasterxml.jackson.databind.PropertyNamingStrategies;
 import com.fasterxml.jackson.databind.annotation.JsonNaming;
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -27,13 +28,20 @@ public class AccommodationEntity {
 
     // @ManyToOne
     private Long tripId;
+
+    @Column(length = 50, nullable = false)
     private String name;
 
     // latitude
+    @Column(nullable = false)
     private Double lat;
     // longitude
+    @Column(nullable = false)
     private Double lng;
 
+    @Column(nullable = false)
     private LocalDateTime checkInDatetime;
+
+    @Column(nullable = false)
     private LocalDateTime checkOutDatetime;
 }
