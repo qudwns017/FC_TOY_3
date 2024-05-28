1717710ae7de9aaaafc1ba3294d24fea4e7c50f6
qudwns017 <qudwns017@naver.com>
2024-05-28 17:36:08 +0900

    Feat: RegisterRequest password Pattern 추가(편한 테스트를 위한 주석처리)

--- src/main/java/org/group6/travel/domain/user/model/request/UserRegisterRequest.java
+++ src/main/java/org/group6/travel/domain/user/model/request/UserRegisterRequest.java
@@ -2,6 +2,7 @@ package org.group6.travel.domain.user.model.request;
 
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,6 +17,8 @@ public class UserRegisterRequest {
     private String email;
 
     @NotBlank(message = "비밀번호를 입력해주세요.")
+//    @Pattern(regexp = "^(?=.*[A-Za-z])(?=.*\\d)(?=.*[@$!%*#?&])[A-Za-z\\d@$!%*#?&]{8,16}$",
+//            message = "비밀번호는 8~16 자리이면서 1개 이상의 알파벳, 숫자, 특수문자를 포함해야합니다.")
     private String password;
 
     @NotBlank(message = "이름을 입력해주세요.")
