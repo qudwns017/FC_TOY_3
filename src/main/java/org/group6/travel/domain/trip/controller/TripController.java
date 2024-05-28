da963ba1766fb3623f72420f676bb8e4a7563483
GOren98 <gangmin9812@gmail.com>
2024-05-28 17:00:10 +0900

    Feat: trip 삽입, 삭제, 수정 기능 추가

--- src/main/java/org/group6/travel/domain/trip/controller/TripController.java
+++ src/main/java/org/group6/travel/domain/trip/controller/TripController.java
@@ -3,6 +3,7 @@ package org.group6.travel.domain.trip.controller;
 import lombok.RequiredArgsConstructor;
 import org.group6.travel.common.api.Api;
 import org.group6.travel.domain.trip.model.dto.TripDto;
+import org.group6.travel.domain.trip.model.dto.TripRequest;
 import org.group6.travel.domain.trip.model.entity.TripEntity;
 import org.group6.travel.domain.trip.service.TripService;
 import org.springframework.web.bind.annotation.*;
@@ -14,6 +15,13 @@ import java.util.List;
 @RequestMapping("/api/trip")
 public class TripController {
     private final TripService tripService;
+
+    @PostMapping
+    public Api<TripEntity> insertTrip(
+            @RequestBody TripRequest tripRequest
+    ){
+        return Api.OK(tripService.insertTrip(tripRequest));
+    }
 
     @GetMapping
     public Api<List<TripEntity>> getTripList(){
@@ -32,5 +40,21 @@ public class TripController {
             @RequestParam("keyword") String keyword
     ){
         return Api.OK(tripService.getTripByKeyword(keyword));
+    }
+
+    @PutMapping("/{tripId}")
+    public Api<TripEntity> updateTrip(
+            @PathVariable Long tripId,
+            @RequestBody TripRequest tripRequest
+    ){
+        return Api.OK(tripService.updateTrip(tripId, tripRequest));
+    }
+
+    @DeleteMapping("/{tripId}")
+    public Api<String> deleteTrip(
+            @PathVariable Long tripId
+    ){
+        tripService.deleteTrip(tripId);
+        return Api.OK("삭제 성공");
     }
 }
--- src/main/java/org/group6/travel/domain/trip/service/TripService.java
+++ src/main/java/org/group6/travel/domain/trip/service/TripService.java
@@ -1,16 +1,32 @@
 package org.group6.travel.domain.trip.service;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.group6.travel.domain.trip.model.dto.TripRequest;
 import org.group6.travel.domain.trip.model.entity.TripEntity;
 import org.group6.travel.domain.trip.repository.TripRepository;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class TripService {
     private final TripRepository tripRepository;
+
+    public TripEntity insertTrip(TripRequest tripRequest){
+        return tripRepository.save(TripEntity.builder()
+                        .userId((long)1)
+                        .tripName(tripRequest.getTripName())
+                        .startDate(tripRequest.getStartDate())
+                        .endDate(tripRequest.getEndDate())
+                        .domestic(tripRequest.getDomestic())
+                        .likeCount(0)
+                        .tripComment(tripRequest.getTripComment())
+                .build());
+    }
 
     public List<TripEntity> getTripAll(){
         return tripRepository.findAll();
@@ -21,5 +37,29 @@ public class TripService {
 
     public List<TripEntity> getTripByKeyword(String keyword){
         return tripRepository.findByTripNameContains(keyword).get();
+    }
+
+    public TripEntity updateTrip(Long tripId, TripRequest tripRequest){
+        Optional<TripEntity> entity = tripRepository.findById(tripId);
+        if(entity.isPresent()){
+            TripEntity saveEntity = entity.get();
+            saveEntity.setTripName(tripRequest.getTripName());
+            saveEntity.setStartDate(tripRequest.getStartDate());
+            saveEntity.setEndDate(tripRequest.getEndDate());
+            saveEntity.setDomestic(tripRequest.getDomestic());
+            saveEntity.setTripComment(tripRequest.getTripComment());
+
+            return tripRepository.save(saveEntity);
+        }
+        return null;
+    }
+
+    public void deleteTrip(Long tripId){
+        Optional<TripEntity> entity = tripRepository.findById(tripId);
+        if(entity.isPresent()) {
+            tripRepository.delete(entity.get());
+        }else{
+           log.info("id 없음");
+        }
     }
 }
