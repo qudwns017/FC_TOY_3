0aa833526cbb8d4b7b5c9a34b9e3fee86390e51e
GOren98 <gangmin9812@gmail.com>
2024-05-29 16:26:19 +0900

    Refactor : like optional refactoring

--- src/main/java/org/group6/travel/domain/like/repository/LikeRepository.java
+++ src/main/java/org/group6/travel/domain/like/repository/LikeRepository.java
@@ -6,13 +6,15 @@ import org.group6.travel.domain.like.model.entity.LikeId;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikeRepository extends JpaRepository<LikeEntity, LikeId> {
 
 
     LikeEntity findByUserIdAndTripId(Long userId, Long tripId); //UserEntity TripEntity
 
-    LikeEntity findByTripId(Long tripId);
+    Optional<LikeEntity> findByTripId(Long tripId);
     //LikeEntity deleteByLikeId(Long LikeId); 취소시 id 필요?
 
 
