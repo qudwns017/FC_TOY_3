c564d5877f7fbaef56b1ff4eca0bc8cf65347d58
cbjoon <qudwns017@naver.com>
2024-05-29 18:20:28 +0900

    Feat: UserDetails 상속한 UserDetailEntity 추가

---
+++ src/main/java/org/group6/travel/domain/token/model/entity/UserDetailsEntity.java
@@ -0,0 +1,52 @@
+package org.group6.travel.domain.token.model.entity;
+
+import java.util.Collection;
+import java.util.List;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.group6.travel.domain.user.model.enums.StoreUserRole;
+import org.group6.travel.domain.user.model.enums.StoreUserStatus;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserDetailsEntity implements UserDetails {
+
+    private Long userId;
+
+    private String email;
+
+    private String password;
+
+    private String userName;
+
+    private StoreUserStatus status;
+
+    private StoreUserRole role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority(this.role.toString()));
+    }
+
+    @Override
+    public String getUsername() {
+        return this.email;
+    }
+
+    @Override
+    public String getPassword() {
+        return this.password;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
+}
