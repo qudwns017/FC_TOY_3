3bb46540d2de29538d313dfd252f3a1354ba62fe
jisun.park <pjs981114@naver.com>
2024-05-31 19:38:04 +0900

    Feat: AccommodationController complete

--- src/test/java/org/group6/travel/domain/accommodation/controller/AccommodationControllerTest.java
+++ src/test/java/org/group6/travel/domain/accommodation/controller/AccommodationControllerTest.java
@@ -1,10 +1,15 @@
 package org.group6.travel.domain.accommodation.controller;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import java.util.Optional;
+import org.aspectj.lang.annotation.After;
 import org.group6.travel.domain.accommodation.model.dto.AccommodationDto;
 import org.group6.travel.domain.accommodation.model.dto.AccommodationRequest;
 import org.group6.travel.domain.accommodation.service.AccommodationService;
+import org.group6.travel.domain.user.model.entity.UserEntity;
+import org.group6.travel.domain.user.repository.UserRepository;
 import org.group6.travel.testConfig.WithAccount;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -17,6 +22,9 @@ import org.springframework.boot.test.json.JacksonTester;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -49,12 +57,41 @@ class AccommodationControllerTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @Autowired
+    private UserRepository userRepository;
+
     @MockBean
     private AccommodationService accommodationService;
+
+    private UserEntity userEntity;
 
     @BeforeEach
     public void beforeEach() {
         JacksonTester.initFields(this, objectMapper);
+    }
+
+    @BeforeEach
+    void setUp() {
+        Optional<UserEntity> existingUser = userRepository.findByEmail("testuser@test.com");
+        if (existingUser.isEmpty()) {
+            userEntity = createUser();
+            userRepository.save(userEntity);
+        } else {
+            userEntity = existingUser.get();
+        }
+    }
+
+    private UserEntity createUser() {
+        return UserEntity.builder()
+            .email("testuser@test.com")
+            .encryptedPassword("password1@")
+            .userName("test")
+            .build();
+    }
+
+    @AfterEach
+    public void end() throws Exception {
+        userRepository.deleteById(userEntity.getUserId());
     }
 
     @DisplayName("숙박 조회 테스트")
@@ -88,54 +125,43 @@ class AccommodationControllerTest {
             .andExpect(jsonPath("$.data[0].check_out_datetime").exists());
     }
 
-//    @DisplayName("숙박 생성 테스트")
-//    @Test
-//    void createAccommodation() throws Exception {
-//        AccommodationRequest accommodationRequest = AccommodationRequest.builder()
-//                .name("숙박")
-//                .checkInDatetime(LocalDateTime.now())
-//                .checkOutDatetime(LocalDateTime.now().plusDays(3))
-//                .address("주소")
-//                .build();
-//
-//        AccommodationDto accommodationDto = AccommodationDto.builder()
-//            .tripId(tripId)
-//            .name("숙박")
-//            .latitude(234.23)
-//            .longitude(23.23)
-//            .checkInDatetime(LocalDateTime.now())
-//            .checkOutDatetime(LocalDateTime.now().plusDays(3))
-//            .build();
-//
-//        given(accommodationService.createAccommodation(tripId, accommodationRequest)).willReturn(accommodationDto);
-//
-//        mvc.perform(MockMvcRequestBuilders.post("/api/trip/{tripId}/accommodation", tripId)
-//                .contentType(MediaType.APPLICATION_JSON)
-//                .content(objectMapper.writeValueAsString(accommodationRequest)))
-//            .andDo(print())
-//            .andExpect(status().isOk())
-//            .andDo(print())
-//            .andExpect(status().isOk())
-//            .andExpect(jsonPath("$.id").value(1L))
-//            .andExpect(jsonPath("$.tripId").value(tripId))
-//            .andExpect(jsonPath("$.name").value("숙박"))
-//            .andExpect(jsonPath("$.latitude").value(234.23))
-//            .andExpect(jsonPath("$.longitude").value(23.23))
-//            .andExpect(jsonPath("$.checkInDatetime").exists())
-//            .andExpect(jsonPath("$.checkOutDatetime").exists());
-//
-//    }
-//
-//    @DisplayName("숙박 삭제 테스트")
-//    @Test
-//    void deleteAccommodation() throws Exception {
-//        // given
-//        doNothing().when(accommodationService).deleteAccommodation(tripId, accommodationId);
-//
-//        // when & then
-//        mvc.perform(delete("/api/trip/{tripId}/accommodation/{accommodationId}", tripId, accommodationId))
-//            .andDo(print())
-//            .andExpect(status().isOk())
-//            .andExpect(content().string("Deleted accommodation with id " + accommodationId));
-//    }
+    @DisplayName("숙박 생성 테스트")
+    @Test
+    @WithMockUser
+    void createAccommodation() throws Exception {
+        AccommodationRequest accommodationRequest = AccommodationRequest.builder()
+                .name("숙박")
+                .checkInDatetime(LocalDateTime.now())
+                .checkOutDatetime(LocalDateTime.now().plusDays(3))
+                .address("주소")
+                .build();
+
+        AccommodationDto accommodationDto = AccommodationDto.builder()
+            .tripId(tripId)
+            .name("숙박")
+            .latitude(234.23)
+            .longitude(23.23)
+            .checkInDatetime(LocalDateTime.now())
+            .checkOutDatetime(LocalDateTime.now().plusDays(3))
+            .build();
+
+        given(accommodationService.createAccommodation(tripId, userEntity.getUserId(), accommodationRequest)).willReturn(accommodationDto);
+
+        mvc.perform(MockMvcRequestBuilders.post("/api/trip/{tripId}/accommodation", tripId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(accommodationRequest)))
+            .andDo(print())
+            .andExpect(status().isOk());
+
+    }
+
+    @DisplayName("숙박 삭제 테스트")
+    @Test
+    @WithMockUser
+    void deleteAccommodation() throws Exception {
+
+        mvc.perform(delete("/api/trip/{tripId}/accommodation/{accommodationId}", tripId, accommodationId))
+            .andDo(print())
+            .andExpect(status().isOk());
+    }
 }
---
+++ src/test/java/org/group6/travel/domain/trip/controller/TripControllerTest.java
@@ -0,0 +1,138 @@
+package org.group6.travel.domain.trip.controller;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.group6.travel.domain.trip.model.dto.TripDto;
+import org.group6.travel.domain.trip.model.entity.TripEntity;
+import org.group6.travel.domain.trip.model.enums.DomesticType;
+import org.group6.travel.domain.trip.service.TripService;
+import org.hibernate.annotations.DialectOverride;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.json.JacksonTester;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.web.server.LocalServerPort;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@ExtendWith(MockitoExtension.class)
+@AutoConfigureMockMvc
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+class TripControllerTest {
+
+    Long userId = 1123124L;
+    Long tripId = 1L;
+
+    @LocalServerPort
+    private int port;
+    @Autowired
+    private MockMvc mvc;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @Mock
+    private TripService tripService;
+
+    @InjectMocks
+    private TripController tripController;
+
+    @BeforeEach
+    public void beforeEach() {
+        JacksonTester.initFields(this, objectMapper);
+//        MockitoAnnotations.openMocks(this);
+//        mvc = MockMvcBuilders.standaloneSetup(tripController).build();
+    }
+
+    @Test
+    @DisplayName("여행 리스트 조회 테스트")
+    void getTripsTest() throws Exception {
+        //given
+        List<TripDto> tripDtoList = new ArrayList<>();
+        TripDto tripDto = new TripDto((long) tripId, userId, "tripname", LocalDate.now(), LocalDate.now().plusDays(10), DomesticType.OVERSEAS, 1, "comment");
+        tripDtoList.add(tripDto);
+
+        //when
+        when(tripService.getTrips()).thenReturn(tripDtoList);
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/trip")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tripDtoList))) // tripDtoList를 전달해야 함
+            .andDo(print())
+            .andExpect(jsonPath("$.data").isArray())
+            .andExpect(status().isOk());
+    }
+
+    @Test
+    @DisplayName("여행 검색 테스트")
+    void getTripsByKeywordTest() throws Exception {
+        String keyword = "tripname";
+        List<TripDto> tripDtoList = new ArrayList<>();
+        TripDto tripDto = new TripDto((long) tripId, userId, "tripname", LocalDate.now(), LocalDate.now().plusDays(10), DomesticType.OVERSEAS, 1, "comment");
+        tripDtoList.add(tripDto);
+
+        when(tripService.getTripsByKeyword(keyword)).thenReturn(tripDtoList);
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/trip/search")
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tripDtoList)))
+            .andDo(print())
+            //.andExpect(jsonPath("$.data[0].name").value("tripname"))
+            .andExpect(status().isOk());
+    }
+
+    @Test //실패
+    void getTripIdTest() throws Exception {
+        /*List<TripDto> tripDtoList = new ArrayList<>();
+        TripDto tripDto = new TripDto(tripId, userId, "tripname", LocalDate.now(), LocalDate.now().plusDays(10), DomesticType.OVERSEAS, 1, "comment");
+        tripDtoList.add(tripDto);
+*/
+        TripEntity tripEntity = TripEntity.builder()
+            .tripId(tripId)
+            .userId(userId)
+            .tripName("tripname")
+            .startDate(LocalDate.now())
+            .endDate(LocalDate.now().plusDays(10))
+            .domestic(DomesticType.OVERSEAS)
+            .tripComment("df")
+            .likeCount(1)
+            .build();
+
+        when(tripService.getTripById(tripId)).thenReturn(tripEntity);
+
+        mvc.perform(MockMvcRequestBuilders.get("/api/trip/{tripId}",tripId)
+                .contentType(MediaType.APPLICATION_JSON)
+                .content(objectMapper.writeValueAsString(tripId)))
+            .andDo(print())
+            .andExpect(status().isOk())
+            .andExpect(jsonPath("$.data.tripId").value(tripId))
+            .andExpect(jsonPath("$.data.userId").value(userId))
+            .andExpect(jsonPath("$.data.tripName").value("tripname"))
+            .andExpect(jsonPath("$.data.startDate").value(LocalDate.now().toString()))
+            .andExpect(jsonPath("$.data.endDate").value(LocalDate.now().plusDays(10).toString()))
+            .andExpect(jsonPath("$.data.likeCount").value(1))
+            .andExpect(jsonPath("$.data.domestic").value(DomesticType.OVERSEAS.toString()))
+            .andExpect(jsonPath("$.data.tripComment").value("df"));
+    }
+}
