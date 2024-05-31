ca5a46259ee761af1112585ef746052b7446a1cd
GOren98 <gangmin9812@gmail.com>
2024-05-31 17:14:40 +0900

    Fix : http status fix

--- src/main/java/org/group6/travel/common/exceptionhandler/ApiExceptionHandler.java
+++ src/main/java/org/group6/travel/common/exceptionhandler/ApiExceptionHandler.java
@@ -4,12 +4,15 @@ import lombok.extern.slf4j.Slf4j;
 import org.group6.travel.common.api.ResponseApi;
 import org.group6.travel.common.exception.ApiException;
 import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
 @Order(value = Integer.MIN_VALUE)
+@ResponseStatus(HttpStatus.METHOD_NOT_ALLOWED)
 public class ApiExceptionHandler {
 
     @ExceptionHandler(value = ApiException.class)
